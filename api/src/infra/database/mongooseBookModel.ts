bd90bfdf2e3fa43361fcde1052a6fa26364f625e
ROBERTA MEYRELLES FRANÇA <roberta.franca@uvvnet.com.br>
2025-07-17 21:43:45 -0300

    feat: configuração da conexão com o MongoDB

---
+++ api/src/infra/database/mongooseBookModel.ts
@@ -0,0 +1,14 @@
+import mongoose, { Schema } from "mongoose";
+
+const bookSchema = new Schema({
+  title: { type: String, require: true },
+  author: { type: String, require: true },
+  publishedAt: { type: Date },
+  format: { type: String },
+  pages: { type: Number },
+  genres: { type: Array },
+  language: { type: String },
+  createdAt: { type: Date, default: Date.now },
+});
+
+export const bookModel = mongoose.model('Book', bookSchema);
--- api/src/infra/server/server.ts
+++ api/src/infra/server/server.ts
@@ -1,11 +1,14 @@
 import express, { Application } from 'express';
 import cors from 'cors';
 import { bookRoutes } from '../../app/routes/book.routes';
+import { connectToMongo } from '../database/mongoConnect';
 
 const app: Application = express();
 
 app.use(express.json());
 app.use(cors());
+
+connectToMongo();
 
 app.use(bookRoutes);
 
